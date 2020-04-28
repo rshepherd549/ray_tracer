7c2e5975d9c9097a12ff0a27afcb49a7059ab200
Richard Shepherd <rshepherd@esri.com>
2020-04-28 22:41:25 +0100

    add tuple operations

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 
+use std::ops::Add;
+use std::ops::AddAssign;
+use std::ops::Div;
+use std::ops::DivAssign;
+use std::ops::Mul;
+use std::ops::MulAssign;
+use std::ops::Neg;
+use std::ops::Sub;
+use std::ops::SubAssign;
+
 trait ApproxEq<Rhs=Self> {
     type Output;
     fn is_approx_eq(self, rhs:Rhs) -> bool;
@@ -29,11 +39,11 @@ fn make_vector(x:f64, y:f64, z:f64) -> Tuple {
 
 impl ApproxEq for Tuple {
     type Output = f64;
-    fn is_approx_eq(self, rhs: Tuple) -> bool {
-        return self.x.is_approx_eq(rhs.x) && 
-               self.y.is_approx_eq(rhs.y) && 
-               self.z.is_approx_eq(rhs.z) && 
-               self.w == rhs.w;
+    fn is_approx_eq(self, other: Tuple) -> bool {
+        return self.x.is_approx_eq(other.x) && 
+               self.y.is_approx_eq(other.y) && 
+               self.z.is_approx_eq(other.z) && 
+               self.w == other.w;
     }
 }
 
@@ -67,6 +77,128 @@ fn test_is_approx_eq() {
     assert!(!is_approx_eq(make_vector(1.1,2.2,3.3), make_vector(2.1,2.2,3.3)));    
 
     assert!(!is_approx_eq(make_point(1.1,2.2,3.3), make_vector(1.1,2.2,3.3)));    
+}
+
+impl Neg for Tuple {
+    type Output = Self;
+    fn neg(self) -> Self {
+        Tuple {x:-self.x, y:-self.y, z:-self.z, w:self.w}
+    }
+}
+
+#[test]
+fn test_neg_tuple() {
+    assert!(is_approx_eq(-make_point(1.2,-3.4,5.6), make_point(-1.2,3.4,-5.6)));
+}
+
+impl Add for Tuple {
+    type Output = Self;
+    fn add(self, other:Self) -> Self {
+        Tuple {x:self.x+other.x, y:self.y+other.y, z:self.z+other.z, w:self.w+other.w}
+    }
+}
+
+impl AddAssign for Tuple {
+    fn add_assign(&mut self, other:Self) {
+        self.x += other.x;
+        self.y += other.y;
+        self.z += other.z;
+        self.w += other.w;
+    }
+}
+
+#[test]
+fn test_add() {
+    assert!(is_approx_eq(make_point(1.0,2.0,3.0) + make_vector(4.0,-5.0,6.0), make_point(5.0,-3.0,9.0)));
+
+    let mut a = make_vector(-2.0,3.5,5.6);
+    a += make_vector(3.5,1.0,2.3);
+    assert!(is_approx_eq(a, make_vector(1.5,4.5,7.9)));
+}
+
+impl Sub for Tuple {
+    type Output = Self;
+    fn sub(self, other:Self) -> Self {
+        Tuple {x:self.x-other.x, y:self.y-other.y, z:self.z-other.z, w:self.w-other.w}
+    }
+}
+
+impl SubAssign for Tuple {
+    fn sub_assign(&mut self, other:Self) {
+        self.x -= other.x;
+        self.y -= other.y;
+        self.z -= other.z;
+        self.w -= other.w;
+    }
+}
+
+#[test]
+fn test_sub() {
+    assert!(is_approx_eq(make_point(1.0,2.0,3.0) - make_vector(4.0,-5.0,6.0), make_point(-3.0,7.0,-3.0)));
+
+    let mut a = make_vector(-2.0,3.5,5.6);
+    a -= make_vector(3.5,1.0,2.3);
+    assert!(is_approx_eq(a, make_vector(-5.5,2.5,3.3)));
+}
+
+impl Mul<f64> for Tuple {
+    type Output = Self;
+    fn mul(self, scale:f64) -> Self {
+        Tuple {x:self.x*scale, y:self.y*scale, z:self.z*scale, w:self.w}
+    }
+}
+
+impl MulAssign<f64> for Tuple {
+    fn mul_assign(&mut self, scale:f64) {
+        self.x *= scale;
+        self.y *= scale;
+        self.z *= scale;
+    }
+}
+
+#[test]
+fn test_mul() {
+    assert!(is_approx_eq(make_point(1.0,2.0,3.0) * 2.0, make_point(2.0,4.0,6.0)));
+
+    let mut a = make_vector(-2.0,3.5,5.6);
+    a *= -0.5;
+    assert!(is_approx_eq(a, make_vector(1.0,-1.75,-2.8)));
+}
+
+impl Div<f64> for Tuple {
+    type Output = Self;
+    fn div(self, scale:f64) -> Self {
+        Tuple {x:self.x/scale, y:self.y/scale, z:self.z/scale, w:self.w}
+    }
+}
+
+impl DivAssign<f64> for Tuple {
+    fn div_assign(&mut self, scale:f64) {
+        self.x /= scale;
+        self.y /= scale;
+        self.z /= scale;
+    }
+}
+
+#[test]
+fn test_div() {
+    assert!(is_approx_eq(make_point(1.0,2.0,3.0) / 2.0, make_point(0.5,1.0,1.5)));
+
+    let mut a = make_vector(-2.0,3.5,5.6);
+    a /= -0.5;
+    assert!(is_approx_eq(a, make_vector(4.0,-7.0,-11.2)));
+}
+
+fn sqr(x: f64) -> f64 {
+    return x * x;
+}
+
+fn abs_sqr(tuple: Tuple) -> f64 {
+    return sqr(tuple.x) + sqr(tuple.y) + sqr(tuple.z); 
+}
+
+fn abs(tuple: Tuple) -> f64 {
+    return sqrt(abs_sqr(tuple)); 
 }
 
 fn main() {
